82885ec08419c1d879a54a5e02fcc141693f7d6e
katyfsy <katyfsy@gmail.com>
2022-07-12 14:53:37 -0400

    Name and email formatter

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -2,6 +2,12 @@ package com.galvanize;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       if (args.length < 1) {
+           System.out.println("Please specify a name and email");
+       } else if (args.length == 1) {
+           System.out.println(String.format("Please specify an email for %s",args[0]));
+       } else {
+           System.out.println(args[0] + " <" + args[1] + ">");
+       }
     }
 }
