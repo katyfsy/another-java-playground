7414c83a04d39ce0194b072f4849e674261c8bbd
katyfsy <katyfsy@gmail.com>
2022-07-12 15:25:56 -0400

    remove redundancy

--- src/main/java/com/galvanize/Application.java
+++ src/main/java/com/galvanize/Application.java
@@ -5,7 +5,7 @@ public class Application {
        if (args.length < 1) {
            System.out.println("Please specify a name and email");
        } else if (args.length == 1) {
-           System.out.println(String.format("Please specify an email for %s",args[0]));
+           System.out.println("Please specify an email for " + args[0]);
        } else {
            System.out.println(args[0] + " <" + args[1] + ">");
        }
